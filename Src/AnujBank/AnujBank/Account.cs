4f85bca0b2d7374f57eac9c0fd68774b9c9721f8
unknown <kumaran@.corporate.thoughtworks.com>
2011-03-01 15:44:36 +0530

    Anuj/Jyotsna - Updated Account Client Relationship

--- Src/AnujBank/AnujBank/Account.cs
+++ Src/AnujBank/AnujBank/Account.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +8,84 @@ namespace AnujBank
 {
     public class Account
     {
+        public Account(AccountId id,Client client)
+        {
+            if (client == null)
+                throw new NoClientException("You must provide client"); 
+            AccountNo = id;
+            Client = client;
+            Client.AddAccount(this);
+        }
+        public AccountId AccountNo { get; private set; }
+        public Client Client { get; private set; }
+        public double Balance { get; set; }
+        public DateTime LastUpdatedDate { get; set; }
+
+        public int GetAccountNumber()
+        {
+            return AccountNo.Id;
+        }
     }
+
+    public class NoClientException : Exception
+    {
+        public NoClientException(string message) : base(message)
+        {
+            
+        }
+    }
+
+    public class Client
+    {
+
+        private ClientID _id;
+        private List<Account> _accounts;
+
+        public Client(ClientID clientId)
+        {
+            _id = clientId;
+            _accounts = new List<Account>();
+        }
+        public Account[] Accounts
+        {
+            get { return this._accounts.ToArray(); }
+
+        }
+        
+        public string GetId()
+        {
+            return _id.Id;
+        }
+
+        public void AddAccount(Account account)
+        {
+            this._accounts.Add(account);
+        }
+
+    }
+    
+    public class ClientID
+    {
+      
+        public ClientID(string id)
+        {
+            Id = id;
+         }
+
+        public string Id { get; set; }
+     
+    }
+    public class AccountId
+    {
+
+        public AccountId(int id)
+        {
+            Id = id;
+        }
+        public int Id { get; set; }
+
+    }
+    
+    
+
 }
--- Src/AnujBank/TestAnujBank/TestAccount.cs
+++ Src/AnujBank/TestAnujBank/TestAccount.cs
@@ -1,11 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using AnujBank;
+using NUnit.Framework;
 
 namespace TestAnujBank
 {
+    [TestFixture]
     public class TestAccount
     {
+        [Test]
+        public void ShouldBeAbleToCreateAnAccount()
+        {
+            var client = new Client(new ClientID("ABCD1234"));
+            var account = new Account(new AccountId(12341234), client)
+                              {Balance = 100.00, LastUpdatedDate = DateTime.Now};
+            Assert.AreEqual(account.GetAccountNumber(), 12341234);
+        }
+        [Test, ExpectedException(typeof(NoClientException))]
+        public void ShouldBeAbleRaiseErrorInCaseNoClientProvided()
+        {
+            var account = new Account(new AccountId(12341234), null) { Balance = 100.00, LastUpdatedDate = DateTime.Now };
+        }
     }
 }
