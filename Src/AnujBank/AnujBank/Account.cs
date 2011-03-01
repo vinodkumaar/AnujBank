943639a9de0040ea0631fc77cebad96fd6b47bcb
unknown <anubhav@.corporate.thoughtworks.com>
2011-03-01 16:05:52 +0530

    Anubhav/Vinod: Checking in build file and moving the tools into solution folder

--- Src/AnujBank/AnujBank/Account.cs
+++ Src/AnujBank/AnujBank/Account.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace AnujBank
+﻿namespace AnujBank
 {
     public class Account
     {
+
+        
     }
 }
