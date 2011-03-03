52b95a41414a1e7c7cbf2ceb84d2d4f37ab8e40e
unknown <kaisarek@.corporate.thoughtworks.com>
2011-03-03 15:32:54 +0530

    Anubhav/Kapil Added Target Account

---
+++ Src/AnujBank/AnujBank/TargetAccount.cs
@@ -0,0 +1,30 @@
+﻿namespace AnujBank
+{
+    public class TargetAccount
+    {
+        private readonly Account account;    
+        
+        private float allocatedPercentage;
+
+        public TargetAccount(Account account, float allocatedPercentage)
+        {
+            this.account = account;
+            this.allocatedPercentage = allocatedPercentage;
+        }
+
+        public int GetAccountNumber()
+        {
+            return account.GetAccountNumber();
+        }
+
+        public double GetAmount()
+        {
+            return account.Balance;
+        }
+
+        public void UpdateBalance(double finalAmount)
+        {
+            account.Balance += (finalAmount * (allocatedPercentage/100));
+        }
+    }
+}
---
+++ Src/AnujBank/TestAnujBank/TestTargetAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using AnujBank;
+using NUnit.Framework;
+
+namespace TestAnujBank
+{
+    [TestFixture]
+    class TestTargetAccount
+    {
+
+        [Test]
+        public void ShouldNotBeAbleToCreateATargetAccountWithAccountAndAllocationPercentage()
+        {
+            var account = new Account(new AccountId(12345678), new ClientId("ABC123"))
+                                  {Balance = 100, LastUpdatedDate = DateTime.Now};
+            var targetAccount = new TargetAccount(account, 2);
+            Assert.AreEqual(targetAccount.GetAccountNumber(), account.GetAccountNumber());
+        }
+
+        [Test]
+        public void ShouldBeAbleToUpdateTheBalance()
+        {
+            var account = new Account(new AccountId(12345678), new ClientId("ABC123")) { Balance = 100, LastUpdatedDate = DateTime.Now };
+            var targetAccount = new TargetAccount(account, 2);
+            targetAccount.UpdateBalance(100);
+            Assert.AreEqual(102, targetAccount.GetAmount());
+        }
+    }
+}
